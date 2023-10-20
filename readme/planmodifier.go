3d88380f63d03f0be4f9f181e3d4091946cb5d37
Josh Beard <joshua.beard@liveoak.bank>
2023-10-19 20:07:30 -0600

    WIP: works even more but not DRY

--- readme/planmodifier.go
+++ readme/planmodifier.go
@@ -21,9 +21,15 @@ type otherStringChanged struct {
 
 // Custom plan modifier to flag a *string* attribute for change if another
 // specified *string* attribute changes.
-func stringChangeIfOtherString(attribute path.Path) planmodifier.String {
+func stringChangeIfOtherString(
+	attribute path.Path,
+	otherField string,
+	checkFrontmatter bool,
+) planmodifier.String {
 	return otherStringChanged{
-		otherAttribute: attribute,
+		otherAttribute:   attribute,
+		otherField:       otherField,
+		checkFrontmatter: checkFrontmatter,
 	}
 }
 
@@ -59,21 +65,70 @@ func (m otherStringChanged) PlanModifyString(
 	req planmodifier.StringRequest,
 	resp *planmodifier.StringResponse,
 ) {
-	var otherConfigValue, otherStateValue types.String
+	var isChanged bool
+	var otherConfigValue, otherPlanValue, otherStateValue types.String
 
+	// The config is loaded to know whether to check frontmatter if the
+	// attribute is not set.
 	resp.Diagnostics.Append(
 		req.Config.GetAttribute(ctx, m.otherAttribute, &otherConfigValue)...)
 
+	// Load state to compare to config value (from attribute or frontmatter).
 	resp.Diagnostics.Append(
 		req.State.GetAttribute(ctx, m.otherAttribute, &otherStateValue)...)
 
-	// If the other attribute is changed, mark this attribute as unknown.
-	if otherConfigValue != otherStateValue && !otherConfigValue.IsNull() {
+	// Load plan to compare to config value (from attribute or frontmatter).
+	resp.Diagnostics.Append(
+		req.Plan.GetAttribute(ctx, m.otherAttribute, &otherPlanValue)...)
+
+	var bodyPlanValue types.String
+	resp.Diagnostics.Append(req.Plan.GetAttribute(ctx, path.Root("body"), &bodyPlanValue)...)
+
+	// If the attribute isn't set, check the body front matter.
+	if m.checkFrontmatter && otherConfigValue.IsNull() {
+		value, diag := valueFromFrontMatter(ctx, bodyPlanValue.ValueString(), m.otherField)
+		if diag != "" {
+			resp.Diagnostics.AddError("Error parsing front matter.", diag)
+
+			return
+		}
+
+		// If the value from frontmatter is not empty, compare it to the
+		// current state.
+		if value != (reflect.Value{}) {
+			tflog.Debug(ctx, fmt.Sprintf(
+				"%s was found in frontmatter with value %s",
+				m.otherAttribute, value))
+
+			// If the value from frontmatter is different from the current
+			// plan, mark this attribute as changed.
+			isChanged = value.Interface().(string) != otherPlanValue.ValueString()
+		} else {
+			tflog.Debug(ctx, fmt.Sprintf(
+				"value for %s was not found in frontmatter",
+				m.otherAttribute))
+		}
+	} else {
+		// If the attribute is set, compare it to the current state and ignore
+		// the frontmatter.
+		tflog.Debug(ctx, "otherStringChanged: not checking front matter")
+		isChanged = otherConfigValue != otherStateValue && !otherStateValue.IsNull()
+	}
+
+	// If the other attribute is changed, mark this attribute as unknown to
+	// trigger a change.
+	if isChanged {
 		tflog.Debug(ctx, fmt.Sprintf(
-			"otherStringChanged: %s otherPlanValue (%s) != otherStateValue (%s)",
+			"otherStringChanged: %s otherConfigValue (%s) != otherStateValue (%s)",
 			m.otherAttribute, otherConfigValue, otherStateValue))
 		resp.PlanValue = types.StringUnknown()
+
+		return
 	}
+
+	// If the other attribute is not changed, set this attribute to the
+	// current plan value.
+	resp.PlanValue = req.PlanValue
 }
 
 // PlanModifyInt64 implements a modifier for planning a change for an
@@ -87,24 +142,24 @@ func (m otherStringChanged) PlanModifyInt64(
 	var isChanged bool
 	var otherConfigValue, otherPlanValue, otherStateValue types.String
 
+	// The config is loaded to know whether to check frontmatter if the
+	// attribute is not set.
 	resp.Diagnostics.Append(
 		req.Config.GetAttribute(ctx, m.otherAttribute, &otherConfigValue)...)
 
+	// Load state to compare to config value (from attribute or frontmatter).
 	resp.Diagnostics.Append(
 		req.State.GetAttribute(ctx, m.otherAttribute, &otherStateValue)...)
 
+	// Load plan to compare to config value (from attribute or frontmatter).
 	resp.Diagnostics.Append(
 		req.Plan.GetAttribute(ctx, m.otherAttribute, &otherPlanValue)...)
 
 	var bodyPlanValue types.String
 	resp.Diagnostics.Append(req.Plan.GetAttribute(ctx, path.Root("body"), &bodyPlanValue)...)
 
-	// var bodyPlanValue types.String
-	// resp.Diagnostics.Append(req.Plan.GetAttribute(ctx, path.Root("body"), &bodyPlanValue)...)
-
 	// If the attribute isn't set, check the body front matter.
 	if m.checkFrontmatter && otherConfigValue.IsNull() {
-		tflog.Info(ctx, fmt.Sprintf("otherStringChanged: checking front matter for %s", m.otherField))
 		value, diag := valueFromFrontMatter(ctx, bodyPlanValue.ValueString(), m.otherField)
 		if diag != "" {
 			resp.Diagnostics.AddError("Error parsing front matter.", diag)
@@ -112,47 +167,42 @@ func (m otherStringChanged) PlanModifyInt64(
 			return
 		}
 
-		tflog.Info(ctx, bodyPlanValue.ValueString())
-
-		tflog.Info(ctx, fmt.Sprintf("%s value: %v", m.otherAttribute, value))
-
 		// If the value from frontmatter is not empty, compare it to the
 		// current state.
 		if value != (reflect.Value{}) {
-			tflog.Info(ctx, fmt.Sprintf("%s was found in frontmatter with value %s", m.otherAttribute, value))
+			tflog.Debug(ctx, fmt.Sprintf(
+				"%s was found in frontmatter with value %s",
+				m.otherAttribute, value))
+
+			// If the value from frontmatter is different from the current
+			// plan, mark this attribute as changed.
 			isChanged = value.Interface().(string) != otherPlanValue.ValueString()
 		} else {
-			tflog.Info(ctx, fmt.Sprintf("%s was not found in frontmatter", m.otherAttribute))
+			tflog.Debug(ctx, fmt.Sprintf(
+				"value for %s was not found in frontmatter",
+				m.otherAttribute))
 		}
-
-		// if value != (reflect.Value{}) {
-		// 	tflog.Debug(ctx, fmt.Sprintf("%s: setting value from front matter", req.Path))
-		// 	// resp.PlanValue = types.StringValue(value.Interface().(string))
-		// 	resp.PlanValue = types.Int64Value(value.Interface().(int64))
-		//
-		// 	return
-		// }
 	} else {
-		tflog.Info(ctx, "otherStringChanged: not checking front matter")
-		isChanged = otherConfigValue != otherStateValue && !otherConfigValue.IsNull()
+		// If the attribute is set, compare it to the current state and ignore
+		// the frontmatter.
+		tflog.Debug(ctx, "otherStringChanged: not checking front matter")
+		isChanged = otherConfigValue != otherStateValue && !otherStateValue.IsNull()
 	}
 
-	tflog.Info(ctx, fmt.Sprintf(
-		"otherStringChanged: isChanged %v %s otherPlanValue (%s) otherConfigValue (%s) otherStateValue (%s)",
-		isChanged, m.otherAttribute, otherPlanValue, otherConfigValue, otherStateValue))
-
-	// If the other attribute is changed, mark this attribute as unknown.
-	// if otherPlanValue == otherStateValue && !otherConfigValue.IsNull() { //&& !otherConfigValue.IsNull() {
+	// If the other attribute is changed, mark this attribute as unknown to
+	// trigger a change.
 	if isChanged {
-		tflog.Info(ctx, fmt.Sprintf(
-			"otherStringChanged: %s otherPlanValue (%s) != otherStateValue (%s)",
+		tflog.Debug(ctx, fmt.Sprintf(
+			"otherStringChanged: %s otherConfigValue (%s) != otherStateValue (%s)",
 			m.otherAttribute, otherConfigValue, otherStateValue))
 		resp.PlanValue = types.Int64Unknown()
 
 		return
 	}
-	resp.PlanValue = req.PlanValue
 
+	// If the other attribute is not changed, set this attribute to the
+	// current plan value.
+	resp.PlanValue = req.PlanValue
 }
 
 // -----------------------------------------------------------------------------
